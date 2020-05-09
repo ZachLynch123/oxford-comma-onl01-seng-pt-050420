9a46c6f844e42eb09c8f822700eac24d4dc8a7d6
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-09 18:43:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
-
+  
+  phrase = ""
+  if array.size == 1 
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+    
+  elsif array.size >= 3 
+    array.each_with_index do |str, index| 
+      if index != array.size - 1
+        str << ", "
+        phrase = phrase + str
+        puts index
+        puts array.size
+      else
+        phrase = phrase + "and #{str}"
+        puts phrase
+      end
+    end
+    return phrase
+  end
 end
+ oxford_comma(["fiddleheads","okra","kohlrabi"])
