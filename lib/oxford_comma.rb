3a1e4caba5af707d48aa8f652a0016291f891d3e
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-09 18:41:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
+def 
 def oxford_comma(array)
-
+  
+  phrase = ""
+  if array.size == 1 
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+    
+  elsif array.size >= 3 
+    array.each_with_index do |str, index| 
+      if index != array.size - 1
+        str << ", "
+        phrase = phrase + str
+        puts index
+        puts array.size
+      else
+        phrase = phrase + "and #{str}"
+        puts phrase
+      end
+      return phrase
+    end
+  end
 end
+ oxford_comma(["fiddleheads","okra","kohlrabi"])
